2aa12fa70b2fc892385a1e23b5789b02865f3f80
Ed Lee <ed@payeasy.com.tw>
2014-05-27 10:10:27 +0800

    1.7 initial files

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 (function(){
     var app = angular.module('gemStore', []);
     app.controller('StoreController', function () {
-        this.product = gem;
+
     })
 
-    var gem = {
-        name: 'Azurite',
-        price: 110.50,
-        canPurchase: false,
-        soldOut: true
-    };
+    var gems = [
+        { name: 'Azurite', price: 2.95 },
+        { name: 'Bloodstone', price: 5.95 },
+        { name: 'Zircon', price: 3.95 },
+    ];
 })();
