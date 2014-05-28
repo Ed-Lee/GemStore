9d356d19fcdb48f9acf07eea74334437315222c7
Ed Lee <ed@payeasy.com.tw>
2014-05-28 17:20:58 +0800

    When `addReview` is called with a product, it should add the `review` from the controller to the passed in products reviews array. Implement this functionality in the `addReview` method.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@
     app.controller('ReviewController', function () {
         this.review = {};
         this.addReview = function (product) {
-            
+            product.reviews.push(this.review);
         }
     });
 
