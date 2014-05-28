50a942a057784dfe2d0668f120896163e6895842
Ed Lee <ed@payeasy.com.tw>
2014-05-28 17:22:35 +0800

    Reset the `review` to an empty object after it's been added to `product.reviews`

--- app.js
+++ app.js
@@ -28,7 +28,8 @@
         this.review = {};
         this.addReview = function (product) {
             product.reviews.push(this.review);
-        }
+            this.review = {};
+        };
     });
 
     var gems = [
