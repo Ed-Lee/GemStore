6b4cbe087169c6781af2deaea5237351d890621d
Ed Lee <ed@payeasy.com.tw>
2014-05-29 11:50:21 +0800

    Create an Element Directive for our `Description` div that includes the `product-description.html`.

--- app.js
+++ app.js
@@ -33,6 +33,13 @@
         };
     });
 
+    app.directive('productDescription', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-description.html'
+        };
+    });
+
     var gems = [
         {
             name: 'Azurite',
